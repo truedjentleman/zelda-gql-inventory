902966d43c68421d82bc70757585d9615a9f4252
I-the-Creator <andrey.burluckiy@gmail.com>
2022-08-28 19:00:06 +0200

    cra boilerplate code deleted; added Hearts and Rupee container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import RupeeContainer from './components/RupeeContainer';
+import HeartsContainer from './components/HeartsContainer';
+
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header className="header-container">
+        <HeartsContainer />
+        <h1>Inventory</h1>
+        <RupeeContainer />
       </header>
+      <div>
+
+      </div>
+      <footer></footer>
     </div>
   );
 }
